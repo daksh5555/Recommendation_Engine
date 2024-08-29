e3af53c86c5d099e2cc3b36b8b0ca99ff491a83f
daksh5555 <dakshbhartdwaj@gmail.com>
2024-08-29 17:36:01 +0530

    Update config.py

--- cache/config.py
+++ cache/config.py
@@ -1,4 +1,4 @@
-DATA_SOURCE_URL = 'https://xyz.com'
+DATA_SOURCE_URL = 'https://xyz.com' # rename with actual url
 CACHE_EXPIRY = 30  # Cache expiration time in seconds
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
