56813bef200ed8703d673c47cbf8b9b8acd6f5e4
daksh5555 <dakshbhartdwaj@gmail.com>
2024-08-29 17:35:18 +0530

    Update config.py

--- cache/config.py
+++ cache/config.py
@@ -1,4 +1,4 @@
-DATA_SOURCE_URL = 'https://api.socialverseapp.com'
+DATA_SOURCE_URL = 'https://xyz.com'
 CACHE_EXPIRY = 30  # Cache expiration time in seconds
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
